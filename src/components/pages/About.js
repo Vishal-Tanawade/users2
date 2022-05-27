153d7c6f8f7a99b09a2685d75a3dd087165adb0f
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-27 07:56:21 +0530

    About , Contact page updated

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,14 +1,23 @@
 import React from 'react'
 //
 const About = () => {
-    return (
-        <div className="container">
-        <div className="py-4">
-          <h1 className='Display-3'>About Page</h1>
-
-        </div>
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1 className='Display-3'>About Page</h1>
+        <p className='lead text-justify'>
+          Qui culpa ea eiusmod exercitation irure eu sint et proident tempor culpa deserunt. Adipisicing incididunt eu cillum occaecat cillum quis culpa commodo qui elit. Excepteur dolore est consequat irure officia esse enim non nisi id.
+          Quis esse nulla nulla cupidatat voluptate ea aliqua do sint exercitation. Sunt consectetur Lorem dolor nisi magna sit. Non eu pariatur nostrud Lorem. Exercitation velit culpa voluptate enim fugiat ipsum. Ullamco commodo aliquip cillum ex officia. Tempor aute esse ad commodo deserunt amet.
+          Velit pariatur culpa eiusmod commodo dolore ipsum sint in laborum nostrud sunt proident eu in. Veniam aliqua officia nisi do anim pariatur ex fugiat nostrud sit nisi occaecat. Labore proident consequat sit commodo excepteur enim ea Lorem excepteur voluptate ea nulla duis sit.
+          Quis id voluptate laborum Lorem consectetur est anim sit ipsum voluptate ullamco ea. Ad officia occaecat sit magna incididunt ipsum Lorem in. Amet veniam elit velit dolore ex proident esse dolore et sint quis. Enim ea elit in incididunt amet eiusmod labore dolore. Voluptate velit nulla eiusmod reprehenderit ipsum quis ipsum ex. Sunt excepteur culpa esse magna ut officia exercitation eiusmod do enim deserunt irure. Exercitation elit minim anim enim amet excepteur aliqua sunt quis eiusmod esse in in proident.
+          Nostrud excepteur qui labore officia nostrud. Non eiusmod nostrud laboris mollit pariatur enim magna cupidatat incididunt commodo amet consequat reprehenderit magna. Non laborum Lorem adipisicing amet ullamco mollit sit aliquip veniam nostrud anim incididunt. Proident ut est est ea cupidatat nisi quis proident. Consequat officia minim ad consectetur minim sunt dolor enim quis esse sit. Duis do et est non ex est labore dolor qui non esse et ea in. Incididunt nulla tempor ipsum fugiat id sit ex id.
+          Esse laboris magna Lorem enim laboris veniam. Consectetur consectetur qui ex enim cupidatat ea eiusmod irure fugiat minim voluptate. Culpa enim anim Lorem incididunt ipsum ea nostrud aute magna ut cillum veniam do exercitation.
+          Veniam culpa consequat ut tempor est. Laboris reprehenderit ex ea et consectetur id sint laboris proident aliqua. Incididunt adipisicing elit proident excepteur cupidatat pariatur. Ipsum anim occaecat reprehenderit proident adipisicing pariatur. Eiusmod enim aliquip excepteur commodo ullamco amet. Ullamco occaecat consectetur nulla laborum nostrud ad veniam dolore est ullamco deserunt. Ut Lorem nostrud Lorem non consectetur ad ipsum nostrud.
+          Do sit id aute do laborum minim laborum sunt adipisicing in duis do. Eiusmod non voluptate pariatur tempor occaecat id nulla tempor. Laborum proident dolor ut id sunt sint. Anim veniam proident deserunt aliqua ea et nostrud enim sint aliquip ad minim duis id. Aute sint voluptate cupidatat minim anim veniam est ea aliqua dolore elit ea id eiusmod. Velit mollit est labore cillum do et officia voluptate ullamco ut est anim voluptate. Est ex sunt consequat est Lorem velit occaecat nulla esse incididunt.
+        </p>
       </div>
-    )
+    </div>
+  )
 }
 
 export default About
