e3dc8c5294f7542e672385cf1884169d6dc774c4
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-27 14:27:20 +0530

    Home.js updated users data will be printed in console

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,20 @@
-//rafce
-import React from 'react'
-
+import React,{useState,useEffect} from "react";
+import axios from "axios";
 const Home = () => {
-  return (
-    <div className="container">
-      <div className="py-4">
-        <h1>Home Page</h1>
-      </div>
-    </div>
-  )
-}
-
-export default Home
+const [users,setUser] = useState([]);
+useEffect(()=>{
+loadUsers();
+},[]);
+const loadUsers = async ()=>{
+const result = await axios.get("http://localhost:3003/users");
+console.log(result);
+};
+return (
+<div className="container">
+<div className="py-4">
+<h1>Home Page</h1>
+</div>
+</div>
+);
+};
+export default Home;
